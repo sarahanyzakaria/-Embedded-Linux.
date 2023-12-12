b0c20c1a9da40e46211e807ef2521a7675fe7db4
sarahany <sarahanyzakaria@gmail.com>
2023-12-12 12:46:27 +0200

    Task5.1

---
+++ AdminLinux_Task5/user_script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+sudo adduser --force-badname Sara_Hany
+sudo groupadd Zakaria
+sudo usermod -aG Zakaria Sara_Hany
+# checking on user and group
+echo
+if grep -q "Zakaria" /etc/group; then
+	echo "the group is exist"
+echo 
+else
+	echo "the group is not exist"
+fi
+echo
+if grep -q "Sara_Hany" /etc/passwd; then
+	echo "the user is exist"
+echo
+else 
+	echo "the user is not exist"
+fi
+echo
+echo "information of user:"
+cat/etc/passwd | grep Sara_Hany
+echo
+echo
+echo "information of group:"
+cat/etc/group | grep Zakaria
+
